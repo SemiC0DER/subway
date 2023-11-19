26f5306566b390a744d804f7d204b351d3b6fc2f
Yoo-yongseok <127306023+Yoo-yongseok@users.noreply.github.com>
2023-11-19 23:52:50 +0900

    게시글 리스트, 게시글 작성 완료(ㅈ버그 수정)

--- app/src/main/java/com/example/whatsub/ListActivity.java
+++ app/src/main/java/com/example/whatsub/ListActivity.java
@@ -4,8 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,20 +19,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -52,105 +37,91 @@ public class ListActivity extends AppCompatActivity {
     // 클릭했을 때 어떤 게시물을 클릭했는지 게시물 번호를 담기 위한 배열
     ArrayList<String> seqList = new ArrayList<>();
 
+    ArrayAdapter<String> arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-// LoginActivity 에서 넘긴 userid 값 받기
+        // LoginActivity 에서 넘긴 userid 값 받기
         userid = getIntent().getStringExtra("userid");
 
-// 컴포넌트 초기화
+        // 컴포넌트 초기화
         listView = findViewById(R.id.listView);
 
-// listView 를 클릭했을 때 이벤트 추가
+        // ListView 클릭 이벤트 처리
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // 어떤 값을 선택했는지 토스트를 뿌려줌
+                Toast.makeText(ListActivity.this, adapterView.getItemAtPosition(i) + " 클릭", Toast.LENGTH_SHORT).show();
 
-// 어떤 값을 선택했는지 토스트를 뿌려줌
-                Toast.makeText(ListActivity.this, adapterView.getItemAtPosition(i)+ " 클릭", Toast.LENGTH_SHORT).show();
-
-// 게시물의 번호와 userid를 가지고 DetailActivity 로 이동
+                // 게시물의 번호와 userid를 가지고 DetailActivity 로 이동
                 Intent intent = new Intent(ListActivity.this, DetailActivity.class);
                 intent.putExtra("board_seq", seqList.get(i));
                 intent.putExtra("userid", userid);
                 startActivity(intent);
-
             }
         });
 
-// 버튼 컴포넌트 초기화
+        // 버튼 컴포넌트 초기화
         reg_button = findViewById(R.id.reg_button);
 
-// 버튼 이벤트 추가
+        // 버튼 이벤트 추가
         reg_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-// userid 를 가지고 RegisterActivity 로 이동
+                // userid 를 가지고 RegisterActivity 로 이동
                 Intent intent = new Intent(ListActivity.this, RegisterActivity.class);
                 intent.putExtra("userid", userid);
                 startActivity(intent);
             }
         });
+
+        // 어댑터 초기화
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titleList);
+        listView.setAdapter(arrayAdapter);
     }
 
-
-    // onResume() 은 해당 액티비티가 화면에 나타날 때 호출됨
     @Override
     protected void onResume() {
         super.onResume();
-// 해당 액티비티가 활성화 될 때, 게시물 리스트를 불러오는 함수를 호출
-        GetBoard getBoard = new GetBoard();
-        getBoard.execute();
+        // 해당 액티비티가 활성화 될 때, 게시물 리스트를 불러오는 함수를 호출
+        getBoardData();
     }
 
+    // Firebase에서 게시물 리스트 가져오기
+    private void getBoardData() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference boardRef = database.getReference("boards");
 
-    // 게시물 리스트를 읽어오는 함수
-    // GetBoard AsyncTask 클래스 수정
-    class GetBoard extends AsyncTask<Void, Void, Void> {
+        boardRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                titleList.clear();
+                seqList.clear();
 
-        @Override
-        protected Void doInBackground(Void... voids) {
-            // Firebase 초기화
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference boardRef = database.getReference("boards");
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String title = snapshot.child("title").getValue(String.class);
+                    String seq = snapshot.child("seq").getValue(String.class);
 
-            boardRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    titleList.clear();
-                    seqList.clear();
-
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        String title = snapshot.child("title").getValue(String.class);
-                        String seq = snapshot.child("seq").getValue(String.class);
-
-                        // 가져온 데이터를 리스트에 추가
+                    if (title != null) {
                         titleList.add(title);
-                        seqList.add(seq);
+                        seqList.add(seq != null ? seq : ""); // Ensure seq is not null before adding
                     }
-
-                    // UI 업데이트 - 어댑터 및 리스트뷰
-                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(ListActivity.this, android.R.layout.simple_list_item_1, titleList);
-                    listView.setAdapter(arrayAdapter);
-                    arrayAdapter.notifyDataSetChanged();
                 }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    // 필요 시 오류 처리
-                }
-            });
-            return null;
-        }
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            // onPostExecute에서 다른 UI 업데이트 작업을 진행할 수도 있음
-        }
+                // 데이터가 변경되었음을 어댑터에 알림
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 필요 시 오류 처리
+                Log.e(TAG, "Firebase 데이터베이스 오류: " + databaseError.getMessage());
+            }
+        });
     }
-
 }
