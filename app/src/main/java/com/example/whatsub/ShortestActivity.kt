b280e82b486357db37759baf87b9fd35056bf4a0
조우진 <vediac2000@gmail.com>
2023-11-29 22:51:42 +0900

    검색 바꿈

--- app/src/main/java/com/example/whatsub/ShortestActivity.kt
+++ app/src/main/java/com/example/whatsub/ShortestActivity.kt
@@ -1,9 +1,11 @@
 package com.example.whatsub
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ShortestActivity : AppCompatActivity(){
@@ -16,8 +18,41 @@ class ShortestActivity : AppCompatActivity(){
         val findroad: Button = findViewById(R.id.find_road)
         val criteria: TextView = findViewById(R.id.textView5)
         val start_st: TextView = findViewById(R.id.start_st)
-        val end_st: TextView = findViewById(R.id.textView7)
+        val dest_st: TextView = findViewById(R.id.textView7)
         val transStations: TextView = findViewById(R.id.cost_trans_staion)
         val allStations: TextView = findViewById(R.id.cost_all_station)
+
+        val pathArray: IntArray? = intent.getIntArrayExtra("path_data")
+        val path: List<Int>? = pathArray?.toList()
+        val title = intent.getStringExtra("title")
+        var startText = intent.getStringExtra("startText")
+        var destText = intent.getStringExtra("destText")
+
+        criteria.setText(title)
+        start_st.setText(startText)
+        dest_st.setText(destText)
+
+
+        val route = path?.let { getStationsRoute(it) }
+        transStations.setText(route?.get(0))
+        allStations.setText(route?.get(1))
+
+        findroad.setOnClickListener {
+            startText = startstation.getText().toString()
+            destText = deststation.getText().toString()
+            val startStation = stationMap[startText] ?: -1
+            val endStation = stationMap[destText] ?: -1
+
+            if (startStation != -1 && endStation != -1) {
+                val intent = Intent(this, SelectActivity::class.java)
+                intent.putExtra("startText", startText)
+                intent.putExtra("destText", destText)
+                startActivity(intent)
+            } else
+                Toast.makeText(this,"입력한 역 이름이 유효하지 않습니다.", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
 }
