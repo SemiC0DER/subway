e5657354305880c0a4d70b09fda79e5ae6bbd263
조우진 <vediac2000@gmail.com>
2023-12-01 18:35:17 +0900

    주석 처리

--- app/src/main/java/com/example/whatsub/SelectActivity.kt
+++ app/src/main/java/com/example/whatsub/SelectActivity.kt
@@ -14,7 +14,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-//결과를 대략적으로 표시하는 창이다.
+//결과를 대략적으로 표시하는 창
 class SelectActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
